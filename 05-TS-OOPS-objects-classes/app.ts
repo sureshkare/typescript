cba29d861526d1147f7e7c331e245b94a2f5e207
rajukare <bangarrajuk23@gmail.com>
2023-02-14 15:56:23 +0530

    fifth commit

--- 05-TS-OOPS-objects-classes/app.ts
+++ 05-TS-OOPS-objects-classes/app.ts
@@ -8,7 +8,35 @@ class Mobile {
            this.color = color;
            this.price = price;
     }
+    public getBrand():string{
+          return this.brand;
+    }
+
+    public setBrand(brand : string):void{
+          this.brand = brand;
+    }
+
+    public getColor():string{
+        return this.color;
+  }
+
+  public setColor(color : string):void{
+    this.color = color;
+}
+
+public getPrice():number{
+    return this.price;
+}
+
+public setPrice(price : number):void{
+    this.price = price;
+}
 }
 
 let mobile = new Mobile("apple", "blue", 23000);
+
+console.log(mobile.getBrand());
+mobile.setBrand("Lenovo");
+mobile.setColor("Black");
+mobile.setPrice(12000);
 console.log(mobile);
