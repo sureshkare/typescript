8d3701a130d8a1f4b7668af2a063bea280e1ad6d
rajukare <bangarrajuk23@gmail.com>
2023-02-14 11:56:51 +0530

    second commit

---
+++ 02-TS-variable-decl/app.ts
@@ -0,0 +1,7 @@
+/*
+    1.each variable name should start with lower case on the other hand classes,
+     interfaces and enum starts with uppercase.
+    2.Camelcase naming convention has to be followed.
+*/
+
+let age:number = 10;
---
+++ 03-TS-Datatypes/app.ts
@@ -0,0 +1,12 @@
+/*
+   string, number, boolean, null, undefined,object, array, void, enum, any
+*/
+
+//string
+
+let employeeName:string = 'Suresh';
+console.log(`Employee Name: ${employeeName}`);
+
+//number
+let employeeAge:number = 25;
+console.log(`Employee Age: ${employeeAge}`);
