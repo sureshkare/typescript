5fdecbf7b7b1fc6248798e5f5f3c063f0d625dd0
rajukare <bangarrajuk23@gmail.com>
2023-02-14 12:22:02 +0530

    third commit

--- 03-TS-Datatypes/app.ts
+++ 03-TS-Datatypes/app.ts
@@ -10,3 +10,44 @@ console.log(`Employee Name: ${employeeName}`);
 //number
 let employeeAge:number = 25;
 console.log(`Employee Age: ${employeeAge}`);
+
+//boolean
+let isManager:boolean = true;
+console.log(`isManager: ${isManager}`);
+
+//null
+let test:null = null;
+console.log(`Test: ${test}`);
+
+//undefined
+
+let projectName:undefined = undefined;
+console.log(`Project Name: ${projectName}`);
+
+//object
+let profile:Object = {
+    name: "Suresh",
+    age: 25
+}
+
+//array
+let colors:string[] = ['black', 'blue', 'white']
+console.log(`Colors: ${colors}`)
+
+//void-- It is used when function returns nothing
+let greet = (name:string):void => {
+      console.log(`Hello ${name}`);
+}
+greet("Suresh");
+
+//Enum
+enum Month {
+    JAN = 'JANUARY',
+    FEB = 'FEBRUARY'
+}
+console.log(`Month: ${Month.JAN}`);
+
+//any
+
+let a:any = 'suresh';
+console.log(a);
